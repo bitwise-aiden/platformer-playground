e5d2ac006e75c3f91bff09e814f04e8366d2c0c4
Aiden Storey <git@storey.dev>
2021-09-18 22:39:26 -0400

    Add InputBuffer for handing buffered input

---
+++ source/utils/input_buffer.gd
@@ -0,0 +1,28 @@
+class_name InputBuffer
+
+
+var __name: String = ""
+
+var __buffer: float = .0
+var __elapsed: float =.0
+
+
+# Lifecycle methods
+
+func _init(name: String, buffer: float = .25) -> void: 
+	self.__name = name
+	
+	self.__buffer = buffer
+
+
+# Public methods
+
+func triggered() -> bool:
+	return self.__elapsed > 0.0 
+
+
+func process(delta: float) -> void: 
+	self.__elapsed = max(0.0, self.__elapsed - delta)
+	
+	if Input.is_action_just_pressed(self.__name):
+		self.__elapsed = self.__buffer
